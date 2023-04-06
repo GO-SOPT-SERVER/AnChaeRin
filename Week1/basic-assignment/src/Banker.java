8d7e91ffb7c53b3a72df4feec18f88874ab21621
chaerin <achrvvv@gmail.com>
2023-04-06 14:00:05 +0900

    review 2-5-1 override

---
+++ Week1/basic-assignment/src/Banker.java
@@ -0,0 +1,15 @@
+public class Banker extends Person {
+    public Banker(String name, int age, String phone) {
+        super(name, age, phone);
+    }
+
+    @Override
+    public void sayHi() {
+        System.out.println("Hi there! My name is " + name + ".\nHow can I help you?");
+    }
+
+    public static void main(String[] args) {
+        Banker banker = new Banker("Markus", 26, "010-9090-8080");
+        banker.sayHi();
+    }
+}
