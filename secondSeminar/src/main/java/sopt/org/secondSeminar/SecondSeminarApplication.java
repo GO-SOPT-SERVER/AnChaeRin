8001d9b1a0e599283abf10695787df62d2821a29
chaerin <achrvvv@gmail.com>
2023-04-08 15:07:12 +0900

    initial setting

---
+++ secondSeminar/src/main/java/sopt/org/secondSeminar/SecondSeminarApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.secondSeminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSeminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSeminarApplication.class, args);
+	}
+
+}
