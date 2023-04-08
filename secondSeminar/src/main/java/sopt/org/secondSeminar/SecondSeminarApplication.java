f69e5c5bceba88b4d4166cd625640d2a481bd953
chaerin <achrvvv@gmail.com>
2023-04-08 17:11:02 +0900

    2 seminar

--- secondSeminar/src/main/java/sopt/org/secondSeminar/SecondSeminarApplication.java
+++ secondSeminar/src/main/java/sopt/org/secondSeminar/SecondSeminarApplication.java
@@ -2,12 +2,17 @@ package sopt.org.secondSeminar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sopt.org.secondSeminar.domain.User;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SecondSeminarApplication {
+    public static ArrayList<User> userList;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecondSeminarApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSeminarApplication.class, args);
+        userList = new ArrayList<>();
+    }
 
 }
