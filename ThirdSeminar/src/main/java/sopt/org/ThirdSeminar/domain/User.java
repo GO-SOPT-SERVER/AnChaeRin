0c523d4b76b1d9d92c5dd10f9db5853b21e2cc02
chaerin <achrvvv@gmail.com>
2023-04-23 12:18:21 +0900

    3-seminar

---
+++ ThirdSeminar/src/main/java/sopt/org/ThirdSeminar/domain/User.java
@@ -0,0 +1,34 @@
+package sopt.org.ThirdSeminar.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Getter
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(String nickname, String email, String password){
+        this.nickname=nickname;
+        this.email=email;
+        this.password=password;
+    }
+
+
+
+}
