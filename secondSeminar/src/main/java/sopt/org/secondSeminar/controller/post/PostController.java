f6f464485f8f84ebf780202f22eb748f86f8749c
chaerin <achrvvv@gmail.com>
2023-05-01 11:32:06 +0900

    refactor: refactor with request mapping

--- secondSeminar/src/main/java/sopt/org/secondSeminar/controller/post/PostController.java
+++ secondSeminar/src/main/java/sopt/org/secondSeminar/controller/post/PostController.java
@@ -7,32 +7,32 @@ import sopt.org.secondSeminar.controller.post.dto.request.RegisterRequestDto;
 import sopt.org.secondSeminar.domain.Post;
 import sopt.org.secondSeminar.service.PostService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static sopt.org.secondSeminar.SecondSeminarApplication.postList;
 
 @RestController
+@RequestMapping("/post")
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
 
 
-    @GetMapping("/post/{postId}")
+    @GetMapping("/{postId}")
     public String getPost(@PathVariable final Long postId) {
         System.out.println("요청 게시물 아이디: " + postId);
 
         return postService.getDetail(postId);
     }
 
-    @GetMapping("/post/search")
+    @GetMapping("/search")
     public List<Post> search(@RequestParam final String title) {
         System.out.println("게시물 제목 검색 인자: " + title);
 
         return postService.search(title);
     }
 
-    @PostMapping("/post")
+    @PostMapping()
     public String register(@RequestBody final RegisterRequestDto request) {
         // 서비스 계층에 유저를 등록하는 메서드를 호출
         Long postId = postService.register(request);
@@ -41,14 +41,14 @@ public class PostController {
         return postId + "번 게시물이 등록되었습니다!";
     }
 
-    @PutMapping("/post/{postId}")
+    @PutMapping("/{postId}")
     public String editPost(@PathVariable final Long postId, @RequestBody final EditRequestDto request) {
         System.out.println("수정 게시물 아이디: " + postId);
 
         return postService.edit(postId, request);
     }
 
-    @DeleteMapping("/post/{postId}")
+    @DeleteMapping("/{postId}")
     public String deletePost(@PathVariable final Long postId) {
         System.out.println("삭제 게시물 아이디: " + postId);
 
