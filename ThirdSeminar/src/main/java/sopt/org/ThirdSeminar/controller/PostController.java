3cd41bb73dc5ac0645a7b5526dc188523cd1938b
chaerin <achrvvv@gmail.com>
2023-05-02 15:30:00 +0900

    feat: implement post create api

--- ThirdSeminar/src/main/java/sopt/org/ThirdSeminar/controller/PostController.java
+++ ThirdSeminar/src/main/java/sopt/org/ThirdSeminar/controller/PostController.java
@@ -1,4 +1,23 @@
 package sopt.org.ThirdSeminar.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import sopt.org.ThirdSeminar.controller.dto.request.PostRequestDto;
+import sopt.org.ThirdSeminar.service.PostService;
+
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/post")
+    public Long create(@RequestBody @Valid final PostRequestDto request) {
+        return postService.create(request);
+    }
 }
