abc7b419220557d80b6e33d2b6cd18934430f879
chaerin <achrvvv@gmail.com>
2023-05-10 15:09:22 +0900

    feat: initialize project

---
+++ fouthSeminar/src/main/java/sopt/org/fouthSeminar/FouthSeminarApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.fouthSeminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FouthSeminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FouthSeminarApplication.class, args);
+	}
+
+}
---
+++ fouthSeminar/src/test/java/sopt/org/fouthSeminar/FouthSeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.fouthSeminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FouthSeminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
