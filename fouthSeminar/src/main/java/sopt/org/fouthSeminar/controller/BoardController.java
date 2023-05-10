eb4dc7fd176c18289f0eac2023c2ba665bf305b0
chaerin <achrvvv@gmail.com>
2023-05-10 15:54:22 +0900

    feat: add controller

---
+++ fouthSeminar/src/main/java/sopt/org/fouthSeminar/controller/BoardController.java
@@ -0,0 +1,26 @@
+package sopt.org.fouthSeminar.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import sopt.org.fouthSeminar.controller.dto.ApiResponse;
+import sopt.org.fouthSeminar.controller.dto.BoardRequestDto;
+import sopt.org.fouthSeminar.exception.Success;
+import sopt.org.fouthSeminar.service.BoardService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse create(@RequestBody @Valid final BoardRequestDto request) {
+        boardService.create(request);
+        return ApiResponse.success(Success.CREATE_BOARD_SUCCESS);
+    }
+}
