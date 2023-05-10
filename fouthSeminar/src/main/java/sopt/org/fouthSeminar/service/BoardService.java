21387ea26ae0a2a20ce6a67e13191427141c6e9c
chaerin <achrvvv@gmail.com>
2023-05-10 15:52:05 +0900

    feat: add service

---
+++ fouthSeminar/src/main/java/sopt/org/fouthSeminar/service/BoardService.java
@@ -0,0 +1,36 @@
+package sopt.org.fouthSeminar.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sopt.org.fouthSeminar.controller.dto.BoardRequestDto;
+import sopt.org.fouthSeminar.domain.Board;
+import sopt.org.fouthSeminar.domain.User;
+import sopt.org.fouthSeminar.exception.Error;
+import sopt.org.fouthSeminar.exception.model.NotFoundException;
+import sopt.org.fouthSeminar.infrastructure.BoardRepository;
+import sopt.org.fouthSeminar.infrastructure.UserRepository;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public void create(BoardRequestDto request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
+
+        Board newBoard = Board.newInstance(
+                user,
+                request.getTitle(),
+                request.getContent(),
+                request.getIsPublic()
+        );
+
+        boardRepository.save(newBoard);
+    }
+}
