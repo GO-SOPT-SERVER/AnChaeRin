2625df5056cac0b073e4325cf95622602744bf1c
chaerin <achrvvv@gmail.com>
2023-05-20 13:52:32 +0900

    feat: implement refresh token logic

--- fouthSeminar/src/main/java/sopt/org/fouthSeminar/controller/BoardController.java
+++ fouthSeminar/src/main/java/sopt/org/fouthSeminar/controller/BoardController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import sopt.org.fouthSeminar.config.jwt.JwtService;
+import sopt.org.fouthSeminar.config.resolver.UserId;
 import sopt.org.fouthSeminar.controller.dto.ApiResponse;
 import sopt.org.fouthSeminar.controller.dto.BoardRequestDto;
 import sopt.org.fouthSeminar.exception.Success;
@@ -23,9 +24,9 @@ public class BoardController {
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse create(
-            @RequestHeader("Authorization") String accessToken,
+            @UserId Long userId,
             @RequestBody @Valid final BoardRequestDto request) {
-        boardService.create(Long.parseLong(jwtService.getJwtContents(accessToken)), request);
+        boardService.create(userId, request);
         return ApiResponse.success(Success.CREATE_BOARD_SUCCESS);
     }
 }
--- fouthSeminar/src/main/java/sopt/org/fouthSeminar/exception/model/UnauthorizedException.java
+++ fouthSeminar/src/main/java/sopt/org/fouthSeminar/exception/model/UnauthorizedException.java
@@ -1,6 +1,5 @@
 package sopt.org.fouthSeminar.exception.model;
 
-import lombok.Getter;
 import sopt.org.fouthSeminar.exception.Error;
 
 public class UnauthorizedException extends SoptException {
